613d0ba68408450704930a1a66b4e7a24494d80c
Noah Tye <hi@noahtye.com>
2018-08-15 15:14:22 -0700

    add menu to choose different docs to edit

--- editor/src/App.js
+++ editor/src/App.js
@@ -22,8 +22,116 @@ var db = firebase.firestore();
 db.settings({timestampsInSnapshots: true});
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      openDoc: null,
+      docList: [],
+    };
+  }
+
+  componentDidMount() {
+    db.collection('dialogues').get().then((snapshot) => {
+      console.log(snapshot);
+      this.setState({
+        loading: false,
+        docList: snapshot.docs.map((doc) => ({id: doc.id, name: doc.data().name}))
+      });
+    })
+  }
+
+  open(listing, evt) {
+    this.setState({openDoc: listing});
+  }
+
+  close() {
+    this.setState({openDoc: null});
+  }
+
+  createNew(name) {
+    // todo: save this in the background and immediately open new doc
+    db.collection('dialogues').add({
+      name: name,
+      nodes: {},
+    }).then((docRef) => {
+      this.setState({
+        openDoc: {id: docRef.id, name: name},
+      });
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>
+        Loading...
+      </div>;
+    } else if (this.state.openDoc === null) {
+      return <div>
+        <div>
+          Create a new hyperdialogue:
+          <DocCreator onEnter={this.createNew.bind(this)}/>
+        </div>
+        <div>
+          Open an existing hyperdialogue:
+          {this.state.docList.map((listing) =>
+            <div
+              style={styles.docMenuItem}
+              key={listing.id}
+              onClick={this.open.bind(this, listing)}>
+              {listing.name}
+            </div>
+          )}
+        </div>
+      </div>;
+    } else {
+      return <div>
+        <div style={{fontSize: 30}}>
+          <span style={{cursor: 'pointer'}} onClick={this.close.bind(this)}>💬</span>
+          <span>{this.state.openDoc.name}</span>
+        </div>
+        <Editor docID={this.state.openDoc.id} docName={this.state.openDoc.name} />
+      </div>
+    }
+  }
+}
+
+class DocCreator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  render() {
+    return <div>
+      <input
+        type="text"
+        value={this.state.title}
+        onChange={(e) => this.setState({title: e.target.value})}
+        />
+      <button onClick={() => this.props.onEnter(this.state.title)}>
+        create
+      </button>
+    </div>
+  }
+}
+
+const styles = {
+  docMenuItem: {
+    padding: 10,
+    margin: 10,
+    border: '1px solid #999',
+    color: '#444',
+    width: 150,
+    cursor: 'pointer',
+  }
+};
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       doc: {},
       selected: null,
@@ -66,8 +174,8 @@ class App extends Component {
   }
 
   clickSave() {
-    db.collection("dialogues").doc("test-00").set({
-      name: "Test 00",
+    db.collection("dialogues").doc(this.props.docID).set({
+      name: this.props.docName,
       nodes: this.state.doc,
     })
     .then(function() {
@@ -79,7 +187,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    db.collection('dialogues').doc('test-00').get().then((doc) => {
+    db.collection('dialogues').doc(this.props.docID).get().then((doc) => {
       if (!doc.exists) {
         console.log('unable to load document');
         return;
